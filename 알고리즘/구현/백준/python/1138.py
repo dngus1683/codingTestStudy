0439e2037825a5c549cc4a5156216e83c457ce87
dngus1683 <dngus1683@naver.com>
2023-08-09 21:41:36 +0900

    구현 - 백준(2개)

---
+++ 알고리즘/구현/백준/python/1138.py
@@ -0,0 +1,62 @@
+# 한 줄로 서기
+# 1 ~ N 까지의 키를 가진 사람 N명이 가로로 줄을 선다.
+# 각자는 왼쪽에 본인보다 키가 큰 사람이 몇명인지 안다.
+# 해당 정보를 통해서 어떻게 줄을 서있는지 구하라.
+
+
+# 내 답안1
+# 메모리 초과 - permutations 결과를 list화 하지 않고 바로 for문에 사용하는 방법으로 해결.
+# 시간: 668ms
+# import sys
+# from itertools import permutations
+# input = sys.stdin.readline
+#
+# N = int(input())
+# li = list(map(int, input().split()))
+#
+# flag = 0
+#
+# for tu in permutations(list(range(1, N+1)), N):
+#     if li[tu[0]-1] != 0:
+#         continue
+#     for i in range(len(tu)):
+#         cnt = 0
+#         for j in range(i-1, -1, -1):
+#             if tu[j] > tu[i]:
+#                 cnt += 1
+#         if li[tu[i]-1] != cnt:
+#             flag = 0
+#             break
+#         else:
+#             flag += 1
+#     if flag == N:
+#         print(*tu)          # *: args, **: kwargs
+#         break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 답안 예시1
+# 나보다 큰 수가 왼쪽에 k개 있다. = 나는 k 번째에 위치해 있다.
+# 시간: 36ms - 내 답안1보다 약 18배 빠름
+N=int(input())
+h=list(map(int,input().split()))[::-1]  # 입력 받은 숫자를 반대로 정렬
+o=[]
+for i in h:
+    o.insert(i,N)   # insert(i,N): N을 i index 자리에 추가. 만약 그 자리에 이미 원소가 있다면 해당 원소를 오른쪽으로 옮기고 추가.
+    N-=1
+print(*o)
---
+++ 알고리즘/구현/백준/python/1262.py
@@ -0,0 +1,80 @@
+# 알파벳 다이아몬드
+# N이 주어질 때, 알파벳 소문자로 다이아몬드를 만든다. 아래 얘시 참조
+# N=3 일 때)
+# ..c..
+# .cbc.
+# cbabc
+# .cbc.
+# ..c..
+# 해당 다이아몬드가 상하좌우로 무한히 연결되어 있다. 이때, 두 좌표 (R1,C1), (R2,C2)가 주어질 때, 해당 좌표에 대한 직사각형 내에 다이아몬드 정보를 구하라.
+
+
+# 내 답안1
+# N이 26 초과인 값일 경우를 고려해야 함.
+# N이 27인 경우, N=1인 경우와 같은 출력을 내서는 안되고, N=26 즉 z인 경우에서 추가로 a가 붙는 결과를 출력해야 한다.
+import sys
+
+input = sys.stdin.readline
+ascNum = 97
+
+N, R1, C1, R2, C2 = map(int, input().split())
+
+for r in range(R1+1, R2+2):
+    for c in range(C1+1, C2+2):
+        Rnum = r % ((N-1)*2+1)
+        Cnum = c % ((N-1)*2+1)
+
+        if Rnum == 0:
+            Rnum = ((N-1)*2+1)
+        if Cnum == 0:
+            Cnum = ((N-1)*2+1)
+
+        if Rnum < N:
+            Rnum = Rnum % N
+        elif Rnum > N:
+            Rnum = N - Rnum % N
+        if Cnum < N:
+            Cnum = Cnum % N
+        elif Cnum > N:
+            Cnum = N - Cnum % N
+
+        num = N*2-Rnum-Cnum
+
+        if num >= N:
+            print('.', end='')
+        elif num >= 26:
+            print(chr(ascNum + num % 26), end='')
+        else:
+            print(chr(ascNum+num), end='')
+    if r == R2+1:
+        break
+    print(' ')
+
+
+
+
+
+
+
+
+
+
+
+# 답안 예시1
+# 내 답안1과 같은 계산으로 다이아몬드를 만들지만 코드가 훨씬 간결하다.
+# 또한, 다이아몬드를 하나 미리 만들고, 좌표를 입력하면서 출력하는 방식으로 코드가 구현되어 있다.
+# n, r1, c1, r2, c2 = map(int, input().split())
+# grid, n1 = 2 * n - 1, n - 1
+#
+#
+# def diamond(x, y):
+#     diff = abs(x % grid - n1) + abs(y % grid - n1)
+#     return chr(diff % 26 + 97) if diff < n else "."
+#
+#
+# print(
+#     "\n".join(
+#         "".join(diamond(i, j) for j in range(c1, c2 + 1)) for i in range(r1, r2 + 1)
+#     )
+# )
+

25293fc447dd4928af1e2a5cd0b0b5d752c97a6d
dngus1683 <dngus1683@naver.com>
2024-03-03 20:02:11 +0900

    총 - python:2개 | DP - 백준(pyhton: 9655, 9656)

---
+++ 알고리즘/DP/백준/python/9655.py
@@ -0,0 +1,13 @@
+# 돌 게임
+# 탁자 위에 돌이 N개 있다. 상근이와 창영이가 한 번에 1 or 3개의 돌을 번갈아 가져갈 때, 마지막 돌을 가져가는 사람이 이기는 게임을 한다.
+# 상근이가 게임을 먼저 시작할 때, 승자를 출력하라.
+
+
+# 내 답안1
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+print("SK" if N%2 else "CY")

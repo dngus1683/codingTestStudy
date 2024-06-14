6688e478153beb124db27b0e14e1d8c16b73a73a
Jangwh <dngus1683@naver.com>
2024-06-14 23:29:21 +0900

    총 - c++: 1개
    수학  - 백준(c++: 2558)

---
+++ 알고리즘/수학/백준/C++/2558.cpp
@@ -0,0 +1,22 @@
+// A+B - 2
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+
+// 내 답안1
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int A = 0;
+	int B = 0;
+
+	cin >> A;
+	cin >> B;
+
+	cout << A + B << endl;
+}
